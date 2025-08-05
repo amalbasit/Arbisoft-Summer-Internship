3bae56c9c008cac15d6a0a909b012f3e612ac978
Amal Basit <amalbasit2004@gmail.com>
2025-08-05 10:34:51 +0500

    More practice examples

--- Basic Python Concepts/unpacking_list_dict.py
+++ Basic Python Concepts/unpacking_list_dict.py
@@ -42,3 +42,45 @@ info2 = {'country': 'Pakistan', 'city': 'Lahore'}
 merged = {**info1, **info2}
 
 print(merged, "\n")
+
+
+
+# Packing values into a tuple using *args
+def pack_tuple(*args):
+    print(args)
+
+pack_tuple(1, 2, 3)
+
+
+# Regular positional args and *args
+def func(a, b, *args):
+    print("a:", a)
+    print("b:", b)
+    print("args:", args)
+
+func(1, 2, 3, 4, 5)
+
+
+
+# Packing key-value pairs into a dictionary
+def func(**kwargs):
+    print(kwargs) 
+
+func(name="Amal", age=25)
+
+
+# Unpacking a dict
+
+def func(name, age):
+    print(name, age)
+
+data = {"name": "Amal", "age": 21}
+func(**data)
+
+
+# Unpacking list values into function parameters
+def add(a, b, c):
+    return a + b + c
+
+nums = [1, 3, 4]
+print(add(*nums))

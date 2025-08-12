0c6cfdf920d73c1ee0b95d44f47d06fefa75e1aa
Amal Basit <amalbasit2004@gmail.com>
2025-08-12 11:46:42 +0500

    Remove deleted files from repo to sync with local state

--- APIs/models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Book(BaseModel):
-    title: str
-    author: str
-    year: int
-    price: float
--- Debugging/debugging_test.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-def remove_duplicates(lst):
-    return list(set(lst)).sort()
-
-nums = [4, 2, 5, 2, 1, 5, 3]
-print(remove_duplicates(nums))
